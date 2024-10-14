c55cc6b1332199fd2b6e2ee152f44290af7d9b85
Anish kumar <anishkr2842003@gmail.com>
2024-10-14 08:18:37 +0530

    mini

--- frontend/src/pages/User/UserDetails.jsx
+++ frontend/src/pages/User/UserDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { UserContext } from "../../utils/userContext";
+import { UserContext } from "../../utils/userContext.jsx";
 
 function UserDetails() {
 
